626f2f85509a87acf7109b6724691849c7c2efea
wemu <werner.mueller@monis.ch>
2020-01-15 22:46:26 +0100

    remove at sign should it be present, added help information

--- src/scripts/moxfun.ts
+++ src/scripts/moxfun.ts
@@ -1,22 +1,30 @@
-import { Robot } from 'hubot'
-import { RocketChatBotAdapter } from 'hubot-rocketchat'
+import {Robot} from 'hubot'
+import {RocketChatBotAdapter} from 'hubot-rocketchat'
 import * as harteier from '../data/harteier.json'
 import * as weicheier from '../data/weicheier.json'
 import * as phrases from '../data/phrases.json'
 
+function removeAtSignFromUsername(username: string): string {
+    return username.replace("@", "")
+}
+
 module.exports = async function moxfun(robot: Robot<RocketChatBotAdapter>) {
+
+    robot.helpCommands().push("!hartei 'Benutzer' - tätuliert 'Benutzer' mit einem Spruch der Macht.")
+    robot.helpCommands().push("!weichei 'Benutzer' - tätuliert 'Benutzer' mit einem Spruch der Schwäche.")
+    robot.helpCommands().push("!phrase - Ein zufälliger Satz zum nachdenken.")
 
     robot.hear(/!hartei (.*)$/i, (res) => {
         const he = res.random(harteier)
         const from = res.message.user.name
-        const to = res.match[1]
+        const to = removeAtSignFromUsername(res.match[1])
         res.send(`@${from} nennt @${to} ${he}`)
     })
 
     robot.hear(/!weichei (.*)$/i, (res) => {
         const we = res.random(weicheier)
         const from = res.message.user.name
-        const to = res.match[1]
+        const to = removeAtSignFromUsername(res.match[1])
         res.send(`@${from} nennt @${to} ${we}`)
     })
 
